278e5e45ec26a9c021f077a6bd5d23c85c11def4
kyaryunha <kyaryunha@naver.com>
2020-09-29 00:40:56 +0900

    Initial Commit

---
+++ MultiLanguage.js
@@ -0,0 +1,51 @@
+/**************************
+ *     Multi Language
+ *     Write by Shin Hyun (kyaryunha)
+ *     Github: https://github.com/kyaryunha
+ **************************/
+(function () {
+     function setLanguage(currentLanguage) {
+        let notCurrentLanguage = currentLanguage === "ko"?"en":"ko";
+        let notCurrentTagNames = document.querySelectorAll("." + notCurrentLanguage);
+         notCurrentTagNames.forEach(function (tag) {
+            if ( !tag.classList.contains("unselect-language")) {
+                tag.classList.add("unselect-language");
+            }
+        });
+        let currentTagNames = document.querySelectorAll("."+currentLanguage);
+         currentTagNames.forEach(function (tag) {
+            if ( tag.classList.contains("unselect-language")) {
+                tag.classList.remove("unselect-language");
+                console.log(tag);
+            }
+        });
+        let selectLanguage = document.getElementById("change-language");
+        selectLanguage.value = currentLanguage;
+    }
+    function changeLanguage(){
+        let selectLanguage = document.getElementById("change-language");
+        let selectLang = selectLanguage.options[selectLanguage.selectedIndex].value;
+        localStorage.setItem('lang',selectLang);
+        setLanguage(selectLang);
+    }
+    function getLanguage(){
+        let language = window.navigator.userLanguage || window.navigator.language;
+        let lang = language.substr(0,2).toLowerCase();
+        let localLang = null;
+        try {
+            localLang = localStorage.getItem('lang');
+        } catch (e) {
+        }
+        if(localLang !== null) lang = localLang;
+        if(lang !== 'ko') lang = 'en';
+        return lang;
+    }
+    document.getElementById("change-language").addEventListener("change", function (e) {
+        changeLanguage();
+    });
+    try{
+        setLanguage(getLanguage());
+    }
+    catch (e) {
+    }
+})();
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # MultiLanguage.js
+
 MultiLanguage.js is Simple Multi Language with Vanilla Javascript
+
+작성자: 신 현( Write by Hyun SHIN )
+
+
+
+## Introduce
+
+이미 다양한 다국어 지원 방법이 있겠지만, 기존 수많은 방법들에 비해 이 프로젝트의 장점은 다음과 같다.
+
+There may already be various multilingual support methods, but the advantages of this project compared to the many existing methods are as follows.
+
+
+
+- HTML에서 해당 텍스트가 나타나는 부분에 번역을 작성할 수 있다.
+- 46줄로 구현된 간단한 JS 파일
+- HTML, CSS, JS만 사용되어 프론트엔드 입문자도 쉽게 사용 가능하다.
+
+
+
+- In HTML, you can write a translation where the text appears.
+- Simple JS file implemented in 46 lines
+- Only HTML, CSS, and JS are used, so even beginners can use it easily.
+
+
+
+## Usage:
+
+#### HTML
+
+<img src="./img/example-html.png" style="width:500px; border-radius: 10px;">
+
+
+
+#### CSS
+
+<img src="./img/example-css.png" style="width:500px; border-radius: 10px;">
+
+
+
+#### JS
+
+##### 5.
+
+<img src="./img/example-js-1.png" style="width:500px; border-radius: 10px;">
+
+
+
+OR
+
+##### 2
+
+<img src="./img/example-js-2.png" style="width:500px; border-radius: 10px;">
